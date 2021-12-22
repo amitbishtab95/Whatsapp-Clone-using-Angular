87ba43fa61b58787ae509b4fdb19d15ecac8e741
Your Name <amitbishtab95@gmail.com>
2021-12-22 11:43:59 +0530

    chat section in making

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,8 +6,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-  @Input() convo;
+  @Input() convo: any;
   constructor() {}
 
   ngOnInit(): void {}
+  submitMessage(event: any) {
+    let value = event.target.value.trim();
+    event.target.value = '';
+    if (value.length < 1) return false;
+    this.convo.messages.unshift({
+      id: 1,
+      body: value,
+      time: '4:00 am',
+      me: true,
+    });
+    return true;
+  }
 }
